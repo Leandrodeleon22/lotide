de642d9a7512271dfd4fa2dcdfe667533ea3a4d3
Leandrodeleon22 <leandrodeleon22@gmail.com>
2023-10-23 07:07:39 -0400

    refactor eqArray

--- eqArrays.js
+++ eqArrays.js
@@ -13,19 +13,20 @@ const assertEqual = function (actual, expected) {
 // assertEqual("same", "same");
 
 const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+
   for (i = 0; i < arr1.length; i++) {
-    if (arr1[i] === arr2[i]) {
-      return true;
-    } else {
+    if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
+  return true;
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
