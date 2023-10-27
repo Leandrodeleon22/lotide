1b33585ad25ef07bb2a6fdbc8d3823d1dcce47ab
Leandrodeleon22 <leandrodeleon22@gmail.com>
2023-10-27 07:53:37 -0400

    eqObject fixed return false immediately

--- eqObjects.js
+++ eqObjects.js
@@ -26,7 +26,7 @@ const eqObjects = function (object1, object2) {
 
   for (const key of objKeysOne) {
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      output = eqArrays(object1[key], object2[key]);
+      if (!eqArrays(object1[key], object2[key])) return false;
     } else {
       if (object1[key] !== object2[key]) {
         return false;
