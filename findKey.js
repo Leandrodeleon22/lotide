2bc2c15a3bf25ea8b8d32e48c5689bb51a6d1e3a
Leandrodeleon22 <leandrodeleon22@gmail.com>
2023-10-28 10:27:54 -0400

    findKey with no key found

--- findKey.js
+++ findKey.js
@@ -10,6 +10,7 @@ const findKey = function (obj, callBackFunc) {
   for (const key of Object.keys(obj)) {
     if (callBackFunc(obj[key])) return key;
   }
+  return "no key found";
 };
 
 assertEqual(
