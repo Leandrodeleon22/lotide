c234105a2468ab4484ebd02c9443e8a3c468dd0a
Leandrodeleon22 <leandrodeleon22@gmail.com>
2023-11-01 07:46:34 -0400

    assertArraysEqual and assertArrayEqual test

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,11 @@
 const middle = require("../middle");
 const assertArrayEqual = require("../assertArraysEqual");
 
-middle([1, 2, 3]); // => [2]
-middle([1, 2, 3, 4, 5]); // => [3]
+// middle([1, 2, 3]); // => [2]
+// middle([1, 2, 3, 4, 5]); // => [3]
 
-middle([1, 2, 3, 4]); // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
+// middle([1, 2, 3, 4]); // => [2, 3]
+// middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
 
 assertArrayEqual(middle([1, 2, 3]), [2]);
 assertArrayEqual(middle([1, 2, 3, 4, 5]), [3]);
