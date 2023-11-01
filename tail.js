70c9e9d23c88d5d09a16c7de46b354fb63cae3d3
Leandrodeleon22 <leandrodeleon22@gmail.com>
2023-11-01 06:54:47 -0400

    tail

--- tail.js
+++ tail.js
@@ -1,26 +1,7 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} `);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} `);
-  }
-};
+const assertEqual = require("./assertEqual");
 
 const tail = function (arr) {
   return arr.slice(1);
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-assertEqual(tail(words)[0], "Lighthouse");
-assertEqual(tail(words).length, words.length - 1);
-
-const oneElement = [2];
-
-console.log(tail(oneElement));
-assertEqual(oneElement.length, 1);
-assertEqual(tail(oneElement)[0], 2);
-assertEqual(tail(oneElement).length, oneElement.length - 1);
-
-assertEqual(tail());
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,15 @@
+const assertEqual = require("../assertEqual");
+const tail = require("../tail");
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+assertEqual(tail(words)[0], "Lighthouse");
+assertEqual(tail(words).length, words.length - 1);
+
+const oneElement = [2];
+
+console.log(tail(oneElement));
+assertEqual(oneElement.length, 1);
+assertEqual(tail(oneElement)[0], 2);
+assertEqual(tail(oneElement).length, oneElement.length - 1);
