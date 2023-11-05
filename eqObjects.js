22350c559f68e3c43dc0713debc4c68b3d159614
leandrodeleon22 <leandrodeleon22@gmail.com>
2023-11-04 23:04:59 -0400

    recursive eqObject add Array.isArray insdie oif its object

--- eqObjects.js
+++ eqObjects.js
@@ -29,10 +29,11 @@ const eqObjects = function (object1, object2) {
       if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       
         if (!eqArrays(object1[key], object2[key])) return false;
-        } 
-      if(!eqObjects(object1[key],object2[key])){
+        } else
+      {
+       if (!eqObjects(object1[key],object2[key])) 
         return false
-      }
+      } 
     
   } else {
       if (object1[key] !== object2[key]) {
