f1680556b753d2787ee24d7b444c8406c784370a
leandrodeleon22 <leandrodeleon22@gmail.com>
2023-11-04 22:18:44 -0400

    recursive eq array

--- eqArrays.js
+++ eqArrays.js
@@ -9,11 +9,20 @@ const assertEqual = require("./assertEqual");
 const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+
+    if(Array.isArray(arr1[i]) && Array.isArray(arr2[i])){
+      if(!eqArrays(arr1[i], arr2[i])){
+        return false
+      } 
+    } 
+
+     else if ( arr1[i] !== arr2[i]) {
       return false;
     }
   }
   return true;
 };
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])) //true
 
 module.exports = eqArrays;
